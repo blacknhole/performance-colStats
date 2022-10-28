5c20e80aca1de69e474e9757139932fcc7ceccc3
mrtsway <wscfx0817@gmail.com>
2022-10-28 09:59:36 +0800

    Benchmark the tool

--- main_test.go
+++ main_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"errors"
+	"io"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -62,5 +64,19 @@ func TestRun(t *testing.T) {
 				t.Errorf("Expected %q, got %q instead", tc.exp, buf.String())
 			}
 		})
+	}
+}
+
+func BenchmarkRun(b *testing.B) {
+	files, err := filepath.Glob("./testdata/benchmark/*.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if err := run(files, "avg", 2, io.Discard); err != nil {
+			b.Error(err)
+		}
 	}
 }
